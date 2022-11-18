4547af6d0cea775161db9774b21709b3d9b17f26
BobadillaLeonRodrigo <al222110815@gmail.com>
2022-11-17 20:35:16 -0600

    import de SELECCIONAR_PRODUCTO y copia de state art

--- context/pedidos/pedidosReducer.js
+++ context/pedidos/pedidosReducer.js
@@ -1,8 +1,15 @@
+import { 
+    SELECCIONAR_PRODUCTO 
+} from '../../types';
 
 
 export default (state,action) => {
     switch (action.type) {
-        
+            case SELECCIONAR_PRODUCTO:
+                return{
+                    ...state,
+                    articulo: action.payload
+                }
         default:
             return state;
     }
--- context/pedidos/pedidosState.js
+++ context/pedidos/pedidosState.js
@@ -4,18 +4,35 @@ import React, { useReducer } from 'react';
 import PedidosReducer from './pedidosReducer';
 import PedidosContext from './pedidosContext';
 
+import { 
+    SELECCIONAR_PRODUCTO 
+} from '../../types';
+
 const PedidosState = props => {
 
     //Crear state inicial
     const initialState = {
-        pedido: []
+        pedido: [],
+        articulo: null
     }
 
     //useReducer con dispatch para ejecutar las funciones
     const [state,dispatch] = useReducer(PedidosReducer, initialState);
 
+    //Seleccionar el productos que el usuario desea ordenar
+    const seleccionarArticulo = articulo => {
+        dispatch({
+            type: SELECCIONAR_PRODUCTO,
+            payload: articulo
+        })
+    }
+
     return (
-        <PedidosContext.Provider value= {{ pedido: state.pedido }} >
+        <PedidosContext.Provider 
+            value= {{ 
+                pedido: state.pedido,
+                articulo: state.articulo,
+                seleccionarArticulo }} >
             {props.children}
         </PedidosContext.Provider>
     )
